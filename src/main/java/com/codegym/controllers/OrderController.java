420c8f3379aa03ebeb1f73d5604acf5e28c7e3d3
unknown <ndanh9698@gmail.com>
2021-11-14 22:45:38 +0700

    DanhNT update show bill list

--- src/main/java/com/codegym/controllers/OrderController.java
+++ src/main/java/com/codegym/controllers/OrderController.java
@@ -1,11 +1,32 @@
 package com.codegym.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codegym.entity.order.Orders;
+import com.codegym.repositories.IOrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/order")
 public class OrderController {
+    @Autowired
+    private IOrdersRepository ordersRepository;
+
+    @GetMapping
+    public ResponseEntity<?> showList(@PageableDefault(value = 5) Pageable pageable,
+            @RequestParam(required = false) String tableCode,
+                                      @RequestParam(required = false) String date){
+        Page<Orders> ordersList = this.ordersRepository.findAllAdv(pageable,tableCode, date) ;
+        if (!ordersList.getContent().isEmpty()){
+            return new ResponseEntity<>(ordersList,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+    }
 }
--- src/main/java/com/codegym/repositories/IOrdersRepository.java
+++ src/main/java/com/codegym/repositories/IOrdersRepository.java
@@ -2,11 +2,28 @@ package com.codegym.repositories;
 
 import com.codegym.entity.order.Orders;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface IOrdersRepository extends JpaRepository<Orders,Integer> {
+    @Query(value = "select o.order_id, create_date, employee_id, o.table_id, fad_id\n" +
+            "from orders o\n" +
+            "join order_detail od on od.order_id = o.order_id\n" +
+            "join `tables` tb on tb.table_id = o.table_id\n" +
+            "where (?1 IS NULL OR tb.table_code like %?1%)\n" +
+            "and (?2 IS NULL OR o.create_date =?2)",
+            countQuery = "select COUNT(*) from orders o\n" +
+                    "join order_detail od on od.order_id = o.order_id\n" +
+                    "join `tables` tb on tb.table_id = o.table_id\n" +
+                    "where (?1 IS NULL OR tb.table_code like %?1%)\n" +
+                    "and (?2 IS NULL OR o.create_date =?2)",
+            nativeQuery = true)
+    Page<Orders> findAllAdv(Pageable pageable, String tableCode, String date);
 }
