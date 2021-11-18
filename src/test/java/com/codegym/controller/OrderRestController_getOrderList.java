0add67423aad8e410292436c5955b3b1a94407bc
unknown <ndanh9698@gmail.com>
2021-11-18 11:13:18 +0700

    DanhNT update

---
+++ src/test/java/com/codegym/controller/OrderRestController_getOrderList.java
@@ -0,0 +1,40 @@
+package com.codegym.controller;
+
+import com.codegym.controllers.OrderController;
+import com.codegym.entity.order.Orders;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+public class OrderRestController_getOrderList {
+    @Autowired
+    private OrderController orderController;
+
+    @Test
+    public void showListOrder_5() {
+        ResponseEntity<Page<Orders>> pageResponseEntity
+                = this.orderController.showList(PageRequest.of(0, 5),"","");
+
+        Assertions.assertEquals(HttpStatus.NOT_ACCEPTABLE, pageResponseEntity.getStatusCode());
+    }
+
+    @Test
+    public void showListOrder_6() {
+        ResponseEntity<Page<Orders>> pageResponseEntity
+                = this.orderController.showList(PageRequest.of(0, 5),"null","null");
+
+        Assertions.assertEquals(HttpStatus.OK, pageResponseEntity.getStatusCode());
+
+        Page<Orders> ordersPage = pageResponseEntity.getBody();
+
+        Assertions.assertEquals(15, ordersPage.getTotalElements());
+        Assertions.assertEquals(3, ordersPage.getTotalPages());
+        Assertions.assertEquals("HD0001", ordersPage.getContent().get(0).getOrderCode());
+    }
+}
