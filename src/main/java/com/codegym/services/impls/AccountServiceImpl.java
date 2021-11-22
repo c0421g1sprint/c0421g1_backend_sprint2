51e385231d31cd589686fcba956d8cdc738df930
manhdung98na <md0949470893@gmail.com>
2021-11-22 21:47:35 +0700

    DungNM 22/11 - Login

--- src/main/java/com/codegym/services/impls/AccountServiceImpl.java
+++ src/main/java/com/codegym/services/impls/AccountServiceImpl.java
@@ -1,24 +1,37 @@
 package com.codegym.services.impls;
 
+import com.codegym.email_java.ConfirmService;
+import com.codegym.email_java.VerifyEmail;
+import com.codegym.email_java.email.EmailSender;
 import com.codegym.entity.account.Account;
 import com.codegym.repositories.IAccountRepository;
 import com.codegym.services.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class AccountServiceImpl implements IAccountService {
     @Autowired
-    IAccountRepository accountRepository;
+    private IAccountRepository accountRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ConfirmService confirmService;
+
+    @Autowired
+    private EmailSender emailSender;
 
     @Override
     public void editPassword(Integer id, String password) {
         password = passwordEncoder.encode(password);
-        accountRepository.editPassword(id,password);
+        accountRepository.editPassword(id, password);
     }
 
     @Override
@@ -29,5 +42,43 @@ public class AccountServiceImpl implements IAccountService {
     @Override
     public Account getAccountByName(String name) {
         return accountRepository.getAccountByName(name);
+    }
+
+    ////DungNM 21/11 - UserDetailService use this function
+    @Override
+    public Account findByUsername(String username) {
+        return this.accountRepository.findAccountByUsername(username);
+    }
+
+    ////DungNM 21/11 - Register account and send Email to verify
+    @Transactional
+    public void signUp(Account account) {
+        Account registerAccount = this.accountRepository.findAccountByUsername(account.getAccountUsername());
+        if (registerAccount != null) {
+            throw new IllegalStateException("username haved already exist");
+        }
+        registerAccount = this.accountRepository.findAccountByEmail(account.getEmail());
+        if (registerAccount != null) {
+            throw new IllegalStateException("email haved already exist");
+        }
+        String encode = passwordEncoder.encode(account.getAccountPassword());
+        account.setAccountPassword(encode);
+        this.accountRepository.signUpAccount(account.getAccountUsername(), account.getAccountPassword(), account.getEmail());
+        registerAccount = this.accountRepository.findAccountByUsername(account.getAccountUsername());
+        this.accountRepository.setRoleForUser(registerAccount.getAccountId());
+        String token = UUID.randomUUID().toString();
+        VerifyEmail verifyEmail = new VerifyEmail(
+                token, registerAccount.getEmail(),
+                LocalDateTime.now(), LocalDateTime.now().plusMinutes(15));
+        this.confirmService.saveTokenSendByEmail(verifyEmail);
+        String link = "http://localhost:8080/api/public/confirm?token=" + token;
+        String contentEmail = emailSender.buildRegisterEmail(link);
+        emailSender.send(registerAccount.getEmail(), contentEmail);
+    }
+
+    //DungNM 21/11 - findAccount By email
+    @Override
+    public Account findAccountByEmail(String email) {
+        return this.accountRepository.findAccountByEmail(email);
     }
 }
