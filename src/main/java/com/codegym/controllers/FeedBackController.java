53d3e617fd496dd02b0e09d255357ae385714e56
unknown <ndanh9698@gmail.com>
2021-11-22 17:09:56 +0700

    DanhNT update 22/11/21 5:10PM

--- src/main/java/com/codegym/controllers/FeedBackController.java
+++ src/main/java/com/codegym/controllers/FeedBackController.java
@@ -21,14 +21,14 @@ public class FeedBackController {
     @Autowired
     IFeedBackService feedBackService;
 
-//    QuanTA codeing back-end search feed back by date 14/11/2021
+    //    QuanTA codeing back-end search feed back by date 14/11/2021
     @GetMapping(value = "/list-by-date/{feedBackDate}")
     public ResponseEntity<Page<FeedBack>> showListFeedBackByDate(@PageableDefault(value = 5) Pageable pageable,
                                                                  @PathVariable(required = false) String feedBackDate) {
-        if (feedBackDate.equals("null")){
+        if (feedBackDate.equals("null")) {
             feedBackDate = null;
         }
-        Page<FeedBack> feedBackPage = feedBackService.findAllFeedBackByDate(feedBackDate,pageable);
+        Page<FeedBack> feedBackPage = feedBackService.findAllFeedBackByDate(feedBackDate, pageable);
         if (feedBackPage.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -37,12 +37,12 @@ public class FeedBackController {
 
     // QuanTA coding findby id feedback 19/11/2021
     @GetMapping(value = "/find-feed-back-by-id/{id}")
-    public ResponseEntity<FeedBack> findFeedBackById (@PathVariable(required = false) Integer id){
-        if (id == null){
+    public ResponseEntity<FeedBack> findFeedBackById(@PathVariable(required = false) Integer id) {
+        if (id == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         FeedBack feedBack = this.feedBackService.findFeedBackById(id);
-        if (feedBack == null){
+        if (feedBack == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             return new ResponseEntity<>(feedBack, HttpStatus.OK);
